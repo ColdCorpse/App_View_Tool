a88a1b1fa9de3e35bf3f68ced6e329185d111f4e
Jordan Hudgens <jordan@devcamp.com>
2017-08-08 16:23:34 +0000

    App tool initial commit

---
+++ lib/App_View_Tool.rb
@@ -0,0 +1,7 @@
+require "App_View_Tool/version"
+require "App_View_Tool/renderer"
+
+
+module AppViewTool
+  # Your code goes here...
+end
---
+++ lib/App_View_Tool/version.rb
@@ -0,0 +1,3 @@
+module AppViewTool
+  VERSION = "0.1.0"
+end
